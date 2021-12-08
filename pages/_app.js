6df082c8ee88f28b9ac13b2bd05098e6b1de8d53
Aaron Nolan <anolan23@yahoo.com>
2021-12-08 12:57:55 -0600

    add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
+import * as gtag from '../lib/gtag';
 import '../sass/_main.scss';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -8,6 +10,22 @@ import { ThemeContext } from '../context/theme';
 function MyApp({ Component, pageProps }) {
   const [isDark, setIsDark] = useState(true);
   const store = useMemo(() => [isDark, setIsDark], [isDark]);
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeContext.Provider value={store}>
       <Navbar />
