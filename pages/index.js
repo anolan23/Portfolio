38eb05ef0dc263593e8529dd4f9ac1908842dcdd
Aaron Nolan <anolan23@yahoo.com>
2022-01-29 12:34:30 -0600

    update fonts. update home page projects

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
       <main className={styles['home__main']}>
         <h1>Hi, I'm Aaron 👋</h1>
         <div className={`${styles['home__main__text--goal']}`}>
-          A web developer looking to join a team.
+          A full stack developer looking to complete your project deadlines
         </div>
         <Link href="/projects">
-          <a className={'link'}>See some of my past projects</a>
+          <a className={'link'}>View my projects</a>
         </Link>
         <div className={styles['home__main__text--tech']}>
           Here are the technologies I use:
@@ -24,30 +24,28 @@ export default function Home() {
         <ol className={styles['home__main__skills']}>
           <li className={styles['home__main__skills__skill']}>Git/Github</li>
           <li className={styles['home__main__skills__skill']}>HTML</li>
-          <li className={styles['home__main__skills__skill']}>CSS</li>
+          <li className={styles['home__main__skills__skill']}>SCSS</li>
           <li className={styles['home__main__skills__skill']}>Javascript</li>
-          <li className={styles['home__main__skills__skill']}>
-            React & Nextjs
-          </li>
+          <li className={styles['home__main__skills__skill']}>Reactjs</li>
+          <li className={styles['home__main__skills__skill']}>Nextjs</li>
           <li className={styles['home__main__skills__skill']}>Redux</li>
-          <li className={styles['home__main__skills__skill']}>Node</li>
+          <li className={styles['home__main__skills__skill']}>Nodejs</li>
           <li className={styles['home__main__skills__skill']}>PostgreSQL</li>
           <li className={styles['home__main__skills__skill']}>MongoDB</li>
-          <li className={styles['home__main__skills__skill']}>GraphQL</li>
-          <li className={styles['home__main__skills__skill']}>Web3js</li>
+          <li className={styles['home__main__skills__skill']}>REST/GraphQL</li>
           <li className={styles['home__main__skills__skill']}>Solidity</li>
         </ol>
         <hr className={styles['home__hr']} />
         <div className={styles['home__main__text--sides']}>
-          A few side projects I am working on:
+          A few full stack projects I am working on:
         </div>
         <div className={styles['home__main__projects']}>
           <div className={styles['home__main__projects__project']}>
             <a
-              href="https://twitleague-next-js.vercel.app"
+              href="https://shoppingify-aaron.herokuapp.com/dashboard"
               className={`${styles['home__main__projects__project__title']} link`}
             >
-              Twitleague
+              Shoppingify
             </a>
             <p className={styles['home__main__projects__project__text']}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
@@ -57,10 +55,10 @@ export default function Home() {
           </div>
           <div className={styles['home__main__projects__project']}>
             <a
-              href="https://yesla.vercel.app"
+              href="https://twitleague-next-js.vercel.app"
               className={`${styles['home__main__projects__project__title']} link`}
             >
-              Yesla
+              Twitleague
             </a>
             <p className={styles['home__main__projects__project__text']}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
