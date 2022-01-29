45d86eb8ab2604c88626c0f81780801f3e142f75
Aaron Nolan <anolan23@yahoo.com>
2022-01-29 12:34:30 -0600

    update fonts. update home page projects

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
       <main className={styles['home__main']}>
         <h1>Hi, I'm Aaron 👋</h1>
         <div className={`${styles['home__main__text--goal']}`}>
-          A web developer looking to join a team.
+          A full stack developer looking to complete your project deadlines
         </div>
         <Link href="/projects">
-          <a className={'link'}>See some of my past projects</a>
+          <a className={'link'}>View my projects</a>
         </Link>
         <div className={styles['home__main__text--tech']}>
           Here are the technologies I use:
@@ -24,48 +24,48 @@ export default function Home() {
         <ol className={styles['home__main__skills']}>
           <li className={styles['home__main__skills__skill']}>Git/Github</li>
           <li className={styles['home__main__skills__skill']}>HTML</li>
-          <li className={styles['home__main__skills__skill']}>CSS</li>
+          <li className={styles['home__main__skills__skill']}>SCSS</li>
           <li className={styles['home__main__skills__skill']}>Javascript</li>
-          <li className={styles['home__main__skills__skill']}>
-            React & Nextjs
-          </li>
+          <li className={styles['home__main__skills__skill']}>Reactjs</li>
+          <li className={styles['home__main__skills__skill']}>Nextjs</li>
           <li className={styles['home__main__skills__skill']}>Redux</li>
-          <li className={styles['home__main__skills__skill']}>Node</li>
+          <li className={styles['home__main__skills__skill']}>Nodejs</li>
           <li className={styles['home__main__skills__skill']}>PostgreSQL</li>
           <li className={styles['home__main__skills__skill']}>MongoDB</li>
-          <li className={styles['home__main__skills__skill']}>GraphQL</li>
-          <li className={styles['home__main__skills__skill']}>Web3js</li>
+          <li className={styles['home__main__skills__skill']}>REST/GraphQL</li>
           <li className={styles['home__main__skills__skill']}>Solidity</li>
         </ol>
         <hr className={styles['home__hr']} />
         <div className={styles['home__main__text--sides']}>
-          A few side projects I am working on:
+          A few full stack projects I am working on:
         </div>
         <div className={styles['home__main__projects']}>
           <div className={styles['home__main__projects__project']}>
             <a
-              href="https://twitleague-next-js.vercel.app"
               className={`${styles['home__main__projects__project__title']} link`}
+              href="https://shoppingify-aaron.herokuapp.com/dashboard"
+              target="_blank"
             >
-              Twitleague
+              Shoppingify
             </a>
             <p className={styles['home__main__projects__project__text']}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              ut odit accusamus eveniet sequi animi nesciunt, praesentium, amet
-              quos fuga esse.
+              Tracks your shopping lists that you've created. A statistics page
+              displays your top items/categories as well as a chart which
+              compares month to month differences.q
             </p>
           </div>
           <div className={styles['home__main__projects__project']}>
             <a
-              href="https://yesla.vercel.app"
               className={`${styles['home__main__projects__project__title']} link`}
+              href="https://twitleague-next-js.vercel.app"
+              target="_blank"
             >
-              Yesla
+              Twitleague
             </a>
             <p className={styles['home__main__projects__project__text']}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              ut odit accusamus eveniet sequi animi nesciunt, praesentium, amet
-              quos fuga esse.
+              A social media application for players, coaches, league owners,
+              and parents to communicate and organize their youth sports
+              leagues.
             </p>
           </div>
         </div>
